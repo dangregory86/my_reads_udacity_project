c2eabc27672255c2cf3b074d2bc06d7a043592ea
Dan Gregory <danielgregory1986@googlemail.com>
2018-02-24 14:00:14 +0000

    Created book component
    Created book component

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 class Search extends Component{
   render(){
-    const {page} = this.props;
+    // const {page} = this.props;
 
     return (
       <div className="search-books">
